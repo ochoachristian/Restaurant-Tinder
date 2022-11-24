a21c0c95e64242fdb4819845838e35b2a041dfc0
Christian <aochoa9698@gmail.com>
2022-11-24 10:43:01 -0500

    if date passed guest will now see a page indicating this

--- frontend/src/Components/Voting/Vote.js
+++ frontend/src/Components/Voting/Vote.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './Vote.module.css'
-import SavedCard from '../Invite/SavedCard'
 import axios from 'axios'
+import moment from 'moment'
 import VotingCard from './VotingCard'
 const API_BASE = 'http://localhost:8081/'
 
@@ -10,10 +10,27 @@ export default function Vote() {
     const pathName = window.location.pathname
     const guestId = pathName.substring(7)
     const [restaurants, setRestaurants] = React.useState()
+    const currentTime = moment()._d
+    const [datePassed, setDatePassed] = React.useState()
+
 
     React.useEffect(() => {
         getRestaurants(guestId)
         }, [])
+
+    React.useEffect(() => {
+        checkDate()
+        }, [!datePassed])
+
+    async function checkDate() {
+        const date = await getDecisionDate()
+        console.log("decision date = " + date + " now = " + currentTime)
+        console.log(!moment(currentTime).isBefore(date))
+        const isBefore = moment(currentTime).isBefore(date) //if date passed it will return false
+        setDatePassed(!isBefore) //set state to true if date passed. boolean will hold false.
+
+       //return !moment(currentTime).isBefore(date)
+    }
 
     async function getInvitationId() {
         const invitationId = await (await axios.get(API_BASE + "guest/" + guestId)).data.invitationId
@@ -38,10 +55,9 @@ export default function Vote() {
         
         const invitationId = await getInvitationId()
         //return console.log(invitationId)
-        
          const res = await axios.get(API_BASE + "invitation/" + invitationId)//grabs invite details
          const decisionDate = res.data.decisionDate
-         console.log(decisionDate)
+      //   console.log(decisionDate)
          return decisionDate 
     }
 
@@ -59,9 +75,20 @@ export default function Vote() {
 
   return (
     <div>
-        <h1 className={styles.h1} onClick={() => getDecisionDate()}>Vote Down Below!</h1>
-        {restaurants && <section className={styles.cardlist}>{display()}</section>}
+
+        { datePassed == false ?
+
+        <div> 
+            <h1 className={styles.h1} onClick={() => console.log("here")}>Vote Down Below!</h1>
+            {restaurants && <section className={styles.cardlist}>{display()}</section>}
+        </div>
+
+        : 
         
+        <h2 className={styles.h2}>Voting period has ended</h2>
+        
+        }
+
     </div>
   )
 }
